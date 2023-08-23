cf67236a781db2df7e70e5f382ab87555ef91f5b
khurram <kh.sheikh021@gmail.com>
2023-08-23 20:21:56 +0500

    done

---
+++ node.js
@@ -0,0 +1,124 @@
+// function abc() {
+//     console.log("Hello world");
+// }
+// abc();
+// //   paramiter
+// function ab(a) {
+//     console.log("Hello" + " " + a);
+// }
+
+// // argument
+// ab("Rafey");
+
+
+
+
+
+
+
+// function clu(num1, num2, oprator) {
+//     if (oprator === "+") {
+//         var clu = num1 + num1;
+//         return clu;
+//     }
+//     else if (oprator === "-") {
+//         var clu = num1 + num2;
+//         return clu;
+//     }
+//     else if (oprator === "*") {
+//         var clu = num1 + num2;
+//         return clu;
+//     }
+//     else if (oprator === "/") {
+//         var clu = num1 + num2;
+//         return clu;
+//     }
+//     else {
+//         console.log("true");
+//     }
+
+
+// };
+
+// clu();
+// var Rafey = clu(2, 3, "-");
+
+// var date = new Date();
+// var days =        [
+//   "sunday",
+//   "monday",
+//   "tuesday",
+//   "wed",
+//   "thursday",
+//   "friday",
+//   "saturday",
+// ];
+// var getDay = date.getDay();
+// console.log(days[getDay]);
+
+// var months = [
+//       "jan",
+//       "feb",
+//       "mar",
+//       "april",
+//       "may",
+//       "june",
+//       "july",
+//       "aug",
+//       "sep",
+//       "oct",
+//       "nov",
+//       "dec",
+//     ];
+//     console.log(months[getMonth]);
+
+// var date = new Date();
+// var getDay = date.getDay();
+
+// console.log(date);
+// var days = ["monday", "tuseday", "wed", "thursday", "friday", "saturday",];
+// var user = prompt();
+// var getMonth = date.getMonth();
+// var months = ["jan", "feb", "mar", "april", "may", "june", "july", "aug", "sep", "oct", "nov", "dec",];
+
+// console.log(months[getMonth]);
+// console.log(days[getDay]);
+
+
+// for (let i = 0; i < days.length; i++) {
+//     for (let j = 0; j < months.length; j++) {
+//         console.log("Month" + i);
+
+//     }
+//     console.log("Days" + i);
+// }
+// if (days !== months) {
+//     alert('yes its okay');
+// } else {
+//     alert("its not okay");
+// }
+
+// function calculation(days, months, oprator) {
+//     if (days === "+") {
+//         var calculation = days + months;
+//         return;
+//     }
+//     else if (days === "-") {
+//         var calculation = days + months;
+//         return;
+//     }
+//     else if (days === "*") {
+//         var calculation = days + months;
+//         return;
+//     }
+//     else if (days === "/") {
+//         var calculation = days + months;
+//         return;
+
+//     }
+// }
+
+// calculation();
+// rafey = calculation();
+// console.log(rafey);
+
